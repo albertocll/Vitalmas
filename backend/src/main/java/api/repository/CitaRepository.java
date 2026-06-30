98fc6e091bfd0c5a2b7e720940260c27713739a1
albertocll <albertoclaroslaboral@gmail.com>
2026-06-30 18:44:32 +0200

    feat: historial de citas por médico

--- backend/src/main/java/api/repository/CitaRepository.java
+++ backend/src/main/java/api/repository/CitaRepository.java
@@ -4,13 +4,13 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import api.model.Cita;
 import api.model.Medico;
 import api.model.Paciente;
 
-@Repository
+
 public interface CitaRepository extends JpaRepository<Cita, UUID> {
     List<Cita> findByMedicoOrderByFechaHoraDesc(Medico medico);
     List<Cita> findByPacienteOrderByFechaHoraDesc(Paciente paciente);
--- backend/src/main/java/api/repository/PacienteRepository.java
+++ backend/src/main/java/api/repository/PacienteRepository.java
@@ -4,11 +4,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import api.model.Paciente;
 
-@Repository
 public interface PacienteRepository extends JpaRepository<Paciente, UUID> {
     Optional<Paciente> findByDni(String dni);
 }
